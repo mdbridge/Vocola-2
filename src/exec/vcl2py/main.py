fd87b445f497dbba0d4f4ea5607e117518702ea0
Mark Lillibridge <mdl@alum.mit.edu>
2015-11-28 10:55:47 -0800

    minor style changes for code climate

--- src/exec/vcl2py/main.py
+++ src/exec/vcl2py/main.py
@@ -2,18 +2,18 @@
 # Main control flow module
 #
 
-VocolaVersion = "2.8.4"
-
-
 import os
 import re
 import sys
 
-from vcl2py.emit import output
-from vcl2py.lex import initialize_token_properties
-from vcl2py.log import *
-from vcl2py.parse import parse_input, check_forward_references
+from vcl2py.emit      import output
+from vcl2py.lex       import initialize_token_properties
+from vcl2py.log       import *
+from vcl2py.parse     import parse_input, check_forward_references
 from vcl2py.transform import transform
+
+
+VocolaVersion = "2.8.4"
 
 
 # ---------------------------------------------------------------------------
@@ -53,7 +53,7 @@ def main_routine():
     #sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)    # <<<>>>
 
     # Debug states: 0 = no info, 1 = show statements, 2 = detailed info
-    Debug                    = 0    
+    Debug                    = 0
     Default_maximum_commands = 1
     Error_encountered        = False
     Force_processing         = False
@@ -76,21 +76,22 @@ def main_routine():
         if   option == "-f":          Force_processing = True; continue
         elif option == "-log_stdout": log_to_stdout    = True; continue
         elif option == "-q":          ignore_INI_file  = True; continue
-        
+
         if len(argv) == 0:
             usage("missing argument for option " + option)
         argument = argv.pop(0)
 
-        if   option == "-debug":        Debug                    = safe_int(argument, 1)
-        elif option == "-extensions":   extensions_file          = argument
-        elif option == "-INI_file":     ini_file                 = argument
-        elif option == "-log_file":     log_file                 = argument
-        elif option == "-max_commands": Default_maximum_commands = safe_int(argument, 1)
-        elif option == "-numbers": 
+        if   option == "-debug":        Debug           = safe_int(argument, 1)
+        elif option == "-extensions":   extensions_file = argument
+        elif option == "-INI_file":     ini_file        = argument
+        elif option == "-log_file":     log_file        = argument
+        elif option == "-max_commands":
+            Default_maximum_commands = safe_int(argument, 1)
+        elif option == "-numbers":
             Default_number_words = {}
             numbers = re.split(r'\s*,\s*', argument.strip())
             i = 0
-            for number in numbers: 
+            for number in numbers:
                 if number != "":
                     Default_number_words[i] = number
                 i = i + 1
@@ -98,25 +99,26 @@ def main_routine():
         else:
             usage("unknown option: " + option)
 
-    if len(argv) == 2: 
+    if len(argv) == 2:
         inputFileOrFolder = argv[0]
         out_folder        = argv[1]
-    else: 
+    else:
         usage()
 
 
     in_file = ""
-    if os.path.isdir(inputFileOrFolder): 
+    if os.path.isdir(inputFileOrFolder):
         # inputFileOrFolder is an entire folder
         In_folder = inputFileOrFolder
-    elif os.path.exists(inputFileOrFolder): 
+    elif os.path.exists(inputFileOrFolder):
         # inputFileOrFolder is a single file
         In_folder, filename = os.path.split(inputFileOrFolder)
         if In_folder == "": In_folder = "."
         in_file, extension  = os.path.splitext(filename)
         if not extension == ".vcl":
-            fatal_error("Input file '" + inputFileOrFolder + "' must end in '.vcl'")
-    else: 
+            fatal_error("Input file '" + inputFileOrFolder +
+                        "' must end in '.vcl'")
+    else:
         fatal_error("Nonexistent input filename '" + inputFileOrFolder + "'")
     if log_file == "": log_file = In_folder + os.sep + "vcl2py_log.txt"
     if ini_file == "": ini_file = In_folder + os.sep + "Vocola.INI"
@@ -127,22 +129,24 @@ def main_routine():
         try:
             set_log(open(log_file, "w"))
         except IOError, e:
-            fatal_error("Unable to open log file '" + log_file + "' for writing: " + str(e))
+            fatal_error("Unable to open log file '" + log_file +
+                        "' for writing: " + str(e))
 
 
     if not ignore_INI_file:   read_ini_file(ini_file)
-    if extensions_file != "": 
+    if extensions_file != "":
         Extension_functions = read_extensions_file(extensions_file)
     else:
         Extension_functions = {}
-    if Debug >= 1: 
-        print_log("default maximum commands per utterance = " + str(Default_maximum_commands))
+    if Debug >= 1:
+        print_log("default maximum commands per utterance = " +
+                  str(Default_maximum_commands))
 
     initialize_token_properties()
     convert_files(in_file, out_folder, suffix)
-    
+
     close_log()
-    if not Error_encountered: 
+    if not Error_encountered:
         if not log_to_stdout: os.remove(log_file)
         sys.exit(0)
     else:
@@ -165,7 +169,7 @@ def read_ini_file(ini_file):
             if not match: continue
             keyword = match.group(1)
             value   = match.group(2)
-            if keyword == "MaximumCommands": 
+            if keyword == "MaximumCommands":
                 Default_maximum_commands = safe_int(value, 1)
     except IOError, e:
         return
@@ -187,7 +191,7 @@ def read_extensions_file(extensions_filename):
             needs_flushing    = safe_int(match.group(4), 1) != 0
             module_name       = match.group(5)
             function_name     = match.group(6)
-            
+
             extension_functions[extension_name] = [minimum_arguments, maximum_arguments, needs_flushing, module_name, function_name]
     except IOError, e:
         pass
@@ -195,13 +199,13 @@ def read_extensions_file(extensions_filename):
 
 
 def expand_in_file(in_file, in_folder):
-    if in_file != "": 
+    if in_file != "":
         # just one file
         return [in_file]
 
     # each .vcl file in folder:
     result = []
-    machine = os.environ.get("COMPUTERNAME", "").lower()  
+    machine = os.environ.get("COMPUTERNAME", "").lower()
     try:
         for filename in os.listdir(in_folder):
             match = re.match(r'^(.+)\.vcl$', filename)
@@ -239,7 +243,7 @@ def convert_file(in_file, out_folder, suffix):
 
     out_file = convert_filename(in_file)
 
-    # The global Module_name is used below to implement application-specific 
+    # The global Module_name is used below to implement application-specific
     # commands in the output Python
     Module_name = out_file.lower()
     # The global Input_name is used below for error logging
@@ -255,16 +259,17 @@ def convert_file(in_file, out_folder, suffix):
         if in_time<out_time and not Force_processing:
             return
 
-    
+
     if Debug>=1: print_log("\n==============================")
 
-    statements, Definitions, Function_definitions, statement_count, error_count, should_emit_dictation_support, file_empty = parse_input(Input_name, In_folder, 
-                                                      Extension_functions, Debug)
-    if error_count == 0: 
+    statements, Definitions, Function_definitions, statement_count, \
+        error_count, should_emit_dictation_support, file_empty \
+        = parse_input(Input_name, In_folder, Extension_functions, Debug)
+    if error_count == 0:
         check_forward_references()
-    
+
     # Prepend a "global" context statement if necessary
-    if len(statements) == 0 or statements[0]["TYPE"] != "context": 
+    if len(statements) == 0 or statements[0]["TYPE"] != "context":
         context            = {}
         context["TYPE"]    = "context"
         context["STRINGS"] = [""]
@@ -272,25 +277,25 @@ def convert_file(in_file, out_folder, suffix):
     #print_log(unparse_statements(statements), True)
     statements = transform(statements, Function_definitions, statement_count)
     #print_log(unparse_statements(statements), True)
-    
+
     # Handle $set directives:
     Maximum_commands = Default_maximum_commands
     Number_words     = Default_number_words
-    for statement in statements: 
-        if statement["TYPE"] == "set": 
+    for statement in statements:
+        if statement["TYPE"] == "set":
             key = statement["KEY"]
-            if key == "MaximumCommands": 
+            if key == "MaximumCommands":
                 Maximum_commands = safe_int(statement["TEXT"], 1)
-            elif key == "numbers": 
+            elif key == "numbers":
                 Number_words = {}
                 numbers = re.split(r'\s*,\s*', statement["TEXT"].strip())
                 i = 0
-                for number in numbers: 
+                for number in numbers:
                     if number != "":
                         Number_words[i] = number
                     i = i + 1
 
-    if error_count > 0: 
+    if error_count > 0:
         if error_count == 1:
             s = ""
         else:
@@ -298,8 +303,8 @@ def convert_file(in_file, out_folder, suffix):
         print_log("  " + str(error_count) + " error" + s + " -- file not converted.")
         Error_encountered = True
         return
-    if file_empty: 
-        # Write empty output file, for modification time comparisons 
+    if file_empty:
+        # Write empty output file, for modification time comparisons
         try:
             OUT = open(out_file, "w")
             OUT.close()
@@ -318,31 +323,31 @@ def convert_file(in_file, out_folder, suffix):
            Number_words, Definitions, Maximum_commands,
            Extension_functions)
 
-# 
-# Warning: this code is very subtle and has a matching inverse function in 
+#
+# Warning: this code is very subtle and has a matching inverse function in
 # _vocola_main.py, getSourceFilename.
 #
 # Ensures:
 #   maps [\w@]* to [\w]*, [-\w@]* to [-\w]*
 #   is invertable
 #   result starts with _ iff input did
-#   does not change any text before the first @ or end of string, whichever 
+#   does not change any text before the first @ or end of string, whichever
 #     comes first
-# 
+#
 def convert_filename(in_file):
     name = in_file
-    
+
     # ensure @ acts as a module name terminator for NatLink
     name = re.sub(r'(.)@', r'\1_@', name)
-    
+
     marker = "e_s_c_a_p_e_d__"
 
     match = re.match(r'([^@]*?)((@(.*))?)$', name)
     module = match.group(1)
     suffix = match.group(2)
-    
+
     if suffix == "" and name.find(marker) == -1: return name
-    
-    suffix = suffix.replace('_','___')
-    suffix = suffix.replace('@','__a_t__')
+
+    suffix = suffix.replace('_', '___')
+    suffix = suffix.replace('@', '__a_t__')
     return module + marker + suffix
