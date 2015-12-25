7c8dffb0f7dd7eb9d67019ec3a26d4d69af53189
Mark Lillibridge <mdl@alum.mit.edu>
2015-12-25 13:41:41 -0800

    various required extensions, main NatLink code

---
+++ src/extensions/vocola_ext_clipboard.py
@@ -0,0 +1,156 @@
+### 
+### Module clipboard:
+### 
+
+import string, time, random
+
+import win32clipboard 
+import win32con
+
+from vocola_ext_variables import *
+
+
+# Opening the clipboard can fail if it is in use, so be prepared to retry:
+def open_clipboard():
+    retries = 300
+    while retries > 0:
+        try:
+            win32clipboard.OpenClipboard()
+            return
+        except:
+            #print "retrying clipboard... " + str(retries)
+            #time.sleep( random.random()/50 )
+            time.sleep( 50 )
+            retries = retries - 1
+    win32clipboard.OpenClipboard()
+
+
+## 
+## Getting and setting the contents of the Windows clipboard as TEXT
+## 
+##   Returns "" (or default if provided) when attempting to retrieve
+##   from a clipboard which does not contain format(s) convertible to
+##   CF_TEXT.
+## 
+##   Note that Windows text uses \r\n instead of \n.
+## 
+
+# Vocola function: Clipboard.Get,0-1
+def clipboard_get(default=""):
+    open_clipboard()
+    result = default
+    if win32clipboard.IsClipboardFormatAvailable(win32con.CF_TEXT):
+       result = win32clipboard.GetClipboardData(win32con.CF_TEXT)
+    win32clipboard.CloseClipboard() 
+    null = string.find(result, chr(0))
+    if null>0:
+        result = result[0:null]
+    return result
+
+# Vocola procedure: Clipboard.Set
+def clipboard_set(aString):
+    open_clipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32con.CF_TEXT, aString) 
+    win32clipboard.CloseClipboard() 
+
+
+## 
+## Same, but instead of using format CF_TEXT, uses format CF_UNICODETEXT
+## 
+##   Encodes Unicode as UTF-8.  UTF-8 (or Unicode) cannot be entered
+## directly in Vocola 2 source code or sent as keyboard input (e.g.,
+## directly or via SendDragonKeys or SendSystemKeys); Dragon
+## NaturallySpeaking and hence Vocola 2 only supports Windows-1252,
+## the default Windows character set.
+## 
+##   You can, however, use Eval to create UTF-8.  For example, given the
+## following Vocola 2 function:
+## 
+##     UTF8(text) := EvalTemplate('u"$text".encode("UTF-8")');
+## 
+## You can write:
+## 
+##     paste Greek Delta    = Clipboard.SetUTF8(UTF8(\u03b4)) {ctrl+v};
+##     paste Greek sentence = Clipboard.SetUTF8(UTF8("A lowercase Greek delta is written \u03b4 and an uppercase one is written \u0394.")) {ctrl+v};
+## 
+##   The input text to UTF8 is in Windows-1252 supplemented with Unicode
+## character specifications of the form \uffff or \Uffffffff where the
+## f's are hexadecimal digits specifying the code point of a Unicode character.
+## 
+
+# Vocola function: Clipboard.GetUTF8
+def clipboard_get_UTF8():
+    open_clipboard()
+    result = ""
+    if win32clipboard.IsClipboardFormatAvailable(win32con.CF_UNICODETEXT):
+       result = win32clipboard.GetClipboardData(win32con.CF_UNICODETEXT)
+    win32clipboard.CloseClipboard() 
+    null = string.find(result, chr(0))
+    if null>0:
+        result = result[0:null]
+    result = result.encode('utf-8')
+    return result
+
+# Vocola procedure: Clipboard.SetUTF8
+def clipboard_set_UTF8(aUTF8String):
+    open_clipboard()
+    win32clipboard.EmptyClipboard()
+    aString = aUTF8String.decode('utf-8')  # throws UnicodeDecodeError
+    win32clipboard.SetClipboardData(win32con.CF_UNICODETEXT, aString) 
+    win32clipboard.CloseClipboard() 
+
+
+## 
+## [Convenience procedures]
+## 
+## Saving and restoring the clipboard (as Unicode encoded as UTF-8)
+## 
+##     If the current clipboard contents do not support the Unicode
+## format, then calling Save then Restore will store "" in the
+## clipboard.
+## 
+## Optional name can be used to save and restore multiple clipboard values.
+## 
+
+# Vocola procedure: Clipboard.Save,0-1
+def clipboard_save(name="save"):
+    variable_set("clipboard:" + name, clipboard_get_UTF8())
+
+# Vocola procedure: Clipboard.Restore,0-1
+def clipboard_restore(name="save"):
+    clipboard_set_UTF8(variable_get("clipboard:" + name))
+
+
+## 
+## Waiting for the clipboard contents to change
+## 
+##     Useful for slow applications (e.g., due to a high latency
+## connection to a remote application).  Typical usage pattern is:
+## 
+##   Clipboard.Set(<unlikely>)
+##   {ctrl+c}
+##   Clipboard.WaitForNew(<unlikely>)
+##   ...
+##   ...Clipboard.Get()...
+## 
+## where <unlikely> is unlikely to be the new contents of the clipboard.
+## 
+
+class Timeout(Exception):
+    pass
+
+# Vocola procedure: Clipboard.WaitForNew,0-2
+def clipboard_wait_for_new(old="", timeout=20):  # timeout in seconds
+    try:
+        timeout = int(timeout)
+    except ValueError:
+        raise ValueError("unable to convert '" + timeout.replace("'", "''") +
+                         "' into an integer")
+    delay = 0.1
+    while timeout > 0:
+        if clipboard_get(old + "!") != old:
+            return
+        time.sleep(delay)
+        timeout -= delay
+    raise Timeout("A timeout occurred while waiting for the clipboard contents to change")
---
+++ src/to_MacroSystem/_vortex.py
@@ -0,0 +1,350 @@
+#
+# code adapted from windict.py, which is copyright 1999 by Joel Gould
+#
+
+import re
+import string
+import sys
+import time
+
+import win32gui
+
+import natlink
+from   natlinkutils import *
+import VocolaUtils
+
+import vocola_ext_clipboard
+import vocola_ext_keys
+
+
+#---------------------------------------------------------------------------
+# VoiceDictation client
+#
+# This class provides a way of encapsulating the voice dictation (DictObj)
+# of NatLink.  We can not derive a class from DictObj because DictObj is an
+# exporeted C class, not a Python class.  But we can create a class which
+# references a DictObj instance and makes it lkook like the class was
+# inherited from DictObj.        
+
+class VoiceDictation:
+
+    def __init__(self):
+        self.dictObj = None
+
+    # Initialization.  Create a DictObj instance and activate it for the
+    # dialog box window.  All callbacks from the DictObj instance will go
+    # directly to the dialog box.
+
+    def initialize(self,dlg):
+        self.dlg = dlg
+        self.dictObj = natlink.DictObj()
+        self.dictObj.setBeginCallback(dlg.onTextBegin)
+        self.dictObj.setChangeCallback(dlg.onTextChange)
+        self.dictObj.activate(dlg.GetSafeHwnd())
+
+    # Call this function to cleanup.  We have to reset the callback
+    # functions or the object will not be freed.
+        
+    def terminate(self):
+        self.dictObj.deactivate()
+        self.dictObj.setBeginCallback(None)
+        self.dictObj.setChangeCallback(None)
+        self.dictObj = None
+
+    # This makes it possible to access the member functions of the DictObj
+    # directly as member functions of this class.
+        
+    def __getattr__(self,attr):
+        try:
+            if attr != '__dict__':
+                dictObj = self.__dict__['dictObj']
+                if dictObj is not None:
+                    return getattr(dictObj,attr)
+        except KeyError:
+            pass
+        raise AttributeError, attr
+
+
+#---------------------------------------------------------------------------
+
+class BasicTextControl:
+    def __init__(self, handle):
+        print "BasicTextControl attaching to window ID 0x%08x" % (handle)
+        print "  with title '%s'" % (win32gui.GetWindowText(handle))
+        self.my_handle = handle
+
+        self.set_buffer_unknown()
+
+        self.dictObj = VoiceDictation()
+        self.dictObj.initialize(self)
+        self.updateState()
+
+    def unload(self):
+        print "unloading BasicTextControl"
+        self.dictObj.terminate()
+        self.dictObj = None
+
+    def GetSafeHwnd(self):
+        return self.my_handle
+
+
+    def set_buffer(self, text):
+        self.fake_prefix = 0
+        # text in application around cursor except for
+        # [0:self.fake_prefix]:
+        self.text = text   
+
+        end = len(text)
+        # application selection:
+        self.app_start, self.app_end = end, end  
+        # what we claim is selected to DNS (can differ due to fake
+        # prefix):
+        self.selStart,  self.selEnd  = end, end  
+        self.keys = ""  # any pending keystrokes for application
+
+        self.showState()
+
+    def set_buffer_unknown(self):
+        # Effectively set state to no capitalization and no space
+        # required before next word, hopefully not affecting language
+        # model any:
+        self.set_buffer("First, ")
+        self.fake_prefix = self.app_end
+
+    def showState(self):
+        text = self.text
+        print "text: '%s<%s>%s'" % (text[:self.selStart], 
+                                    text[self.selStart:self.selEnd],
+                                    text[self.selEnd:])
+        if self.selStart != self.app_start or self.selEnd != self.app_end:
+            print "      '%s[%s]%s'" % (text[:self.app_start], 
+                                        text[self.app_start:self.app_end],
+                                        text[self.app_end:])
+        if self.keys != "":
+            print "  postponed keys: '%s'" % (self.keys)
+
+
+
+    #
+    # Application control routines
+    #
+    # Invariant: after sending self.keys to application, the selection
+    #            is [self.app_start,self.app_end) with the cursor
+    #            before self.app_end.
+    #
+    # Invariants: self.app_{start,end} >= self.fake_prefix
+    #             self.app_start <= self.app_end
+    #
+
+    # returns end-start except counts each \r\n as 1 character:
+    def distance(self, start, end):
+        sign = 1
+        if end < start:
+            sign = -1
+            start, end = end, start
+        size = len(self.text[start:end].replace("\r", ""))
+        return size * sign
+
+    def unselect(self):
+        size = self.distance(self.app_start, self.app_end) # selection size
+        if size > 0:
+            self.keys += "{shift+left " + str(size) + "}"
+            self.app_end = self.app_start
+
+    def move_to(self, target):
+        self.unselect()
+        move = self.distance(self.app_end, target)
+        if move > 0:
+            self.keys += "{right %d}" % (move)
+        elif move < 0:
+            self.keys += "{left %d}"  % (-move)
+        self.app_start = self.app_end = target
+
+    def select(self, start, end):
+        # do not allow selecting fake prefix:
+        start = max(self.fake_prefix, start)
+        end   = max(self.fake_prefix, end)
+
+        if start != self.app_start or end != self.app_end:
+            self.move_to(start)
+            size = self.distance(start, end)
+            self.keys += "{shift+right " + str(size) + "}"
+            self.app_end = end
+
+    def replace(self, start, end, new_text):
+        if start < self.fake_prefix and end >= self.fake_prefix:
+            fake_deletion = self.text[start:self.fake_prefix]
+            if new_text.startswith(fake_deletion):
+                print "nop overwrite of fake prefix ignored"
+                start = max(self.fake_prefix, start)
+                new_text = new_text[len(fake_deletion):]
+
+        if start < self.fake_prefix and start != end:
+            print
+            print "***** ATTEMPT TO DELETE (PART OF) FAKE PREFIX DENIED!"
+            print
+        if end   < self.fake_prefix and new_text != "":
+            print
+            print "***** ATTEMPT TO INSERT IN FAKE PREFIX DENIED!"
+            print
+        start = max(self.fake_prefix, start)
+        end   = max(self.fake_prefix, end)
+
+        size = self.distance(start, end)
+        if size != 0:
+            self.move_to(end)
+            self.keys += "{backspace " + str(size) + "}"
+            self.text = self.text[0:start] + self.text[end:]
+            self.app_start = self.app_end = start
+        if new_text != "":
+            self.move_to(start)
+            self.keys += new_text.replace("\r", "").replace("{", "{{}")
+            self.text = self.text[0:start] + new_text + self.text[start:]
+            self.app_start = self.app_end = start + len(new_text)
+
+    def play_string(self,keys):
+        # the following does not work because it causes onTextChange to be called:
+        #natlink.playString("{shift}" + keys)
+
+        keys = keys.replace("\n", "{enter}")
+        vocola_ext_keys.send_input(keys)
+
+
+    #
+    # Routines for interacting with DNS:
+    #
+
+    # We get this callback just before recognition starts. This is our
+    # chance to update the dictation object just in case we missed a change
+    # made to the edit control.
+    def onTextBegin(self,moduleInfo):
+        self.updateState()
+        pass
+
+    # This subroutine transfers the contents and state of the edit control
+    # into the dictation object.  We currently don't bother to indicate
+    # exactly what changed.  The dictation object will compare the text we
+    # write with the contents of its buffer and only make the necessary
+    # changes (as long as on one contigious region has changed).
+    def updateState(self):
+        #print "updating state..."
+
+        self.dictObj.setLock(1)
+        self.dictObj.setText(self.text, 0, 0x7FFFFFFF)
+        self.dictObj.setTextSel(self.selStart, self.selEnd)
+        # assume everything except fake prefix is visible all the time:
+        visStart,visEnd = self.fake_prefix, len(self.text)
+        self.dictObj.setVisibleText(visStart, visEnd)
+        self.dictObj.setLock(0)
+
+
+    # We get this callback when something in the dictation object changes
+    # like text is added or something is selected by voice.  We then update
+    # the edit control to match the dictation object.
+    def onTextChange(self,delStart,delEnd,newText,selStart,selEnd):
+        #self.dictObj.setLock(1)
+        print "onTextChange %d,%d,%s,%d,%d" % (delStart,delEnd,newText,selStart,selEnd)
+        #print "onTextChange %d,%d,%s,%d,%d" % (delStart,delEnd,repr(newText),selStart,selEnd)
+
+        self.replace(delStart, delEnd, newText)
+        self.select(selStart, selEnd)
+        self.selStart = selStart
+        self.selEnd   = selEnd
+
+        if delStart==delEnd and newText=="":
+            # give spelling window time to pop up if it's going to:
+            time.sleep(.1)
+
+        keys = self.keys
+        handle = win32gui.GetForegroundWindow()
+        if handle == self.my_handle:
+            print "sending: " + keys
+            self.play_string(keys)
+            keys = ""
+        else:
+            print "WARNING: delaying keys due to different active window, window ID 0x%08x" % (handle)
+
+        self.keys = keys
+        self.showState()
+        print "end onTextChange"
+        #self.dictObj.setLock(0)
+
+
+
+#---------------------------------------------------------------------------
+# Command grammar
+
+class CommandGrammar(GrammarBase):
+
+    gramSpec = """
+        <start> exported = vortex (on | off);
+        <load>  exported = vortex (load | line);
+    """
+
+    def initialize(self):
+        print "Init"
+        self.basic_text_control = None
+        self.load(self.gramSpec)
+        self.activateAll()
+    
+    def terminate(self):
+        print "Exit"
+        if self.basic_text_control:
+            self.basic_text_control.unload()
+            self.basic_text_control = None
+        self.unload()
+
+
+    def gotResults_start(self,words,fullResults):
+        option = words[1]
+        if option == "on":
+            if self.basic_text_control:
+                self.basic_text_control.unload()
+            handle = win32gui.GetForegroundWindow()
+            self.basic_text_control = BasicTextControl(handle)
+        elif option == "off":
+            if self.basic_text_control:
+                self.basic_text_control.unload()
+                self.basic_text_control = None
+
+    def gotResults_load(self,words,fullResults):
+        # "vortex on":
+        if self.basic_text_control:
+            self.basic_text_control.unload()
+        handle = win32gui.GetForegroundWindow()
+        self.basic_text_control = BasicTextControl(handle)
+
+        option = words[1]
+        if option == "load":
+            selector = "{ctrl+home}{ctrl+shift+end}"
+            unselect = "{ctrl+end}"
+        elif option == "line":
+            selector = "{home}{shift+end}"
+            unselect = "{end}"
+        natlink.playString("{shift}" + selector + "{ctrl+Ins}" + unselect)
+        time.sleep(.1)
+        text = vocola_ext_clipboard.clipboard_get()
+        print "loaded: "+ repr(text)
+        self.basic_text_control.set_buffer(text)
+
+
+
+## 
+## Starting up and stopping our command grammar:
+## 
+
+command = CommandGrammar()
+command.initialize()
+
+def reset_buffer(argument):
+    if command.basic_text_control:
+        if command.basic_text_control.my_handle == win32gui.GetForegroundWindow():
+            command.basic_text_control.set_buffer_unknown()
+VocolaUtils.callback = reset_buffer  # <<<>>>
+
+def unload():
+    global command
+    if command:
+        command.terminate()
+    command = None
+    VocolaUtils.callback = VocolaUtils.do_nothing
