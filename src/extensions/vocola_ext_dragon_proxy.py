78149835ca675358f91905268c64a65b80de0c8b
Mark Lillibridge <mdl@alum.mit.edu>
2021-11-26 14:25:25 -0800

    initial version of DragonProxy, which supports optional verbose mode and switching between SendInput and SendDragonKeys as the default key sending method

---
+++ src/extensions/vocola_ext_dragon_proxy.py
@@ -0,0 +1,53 @@
+### 
+### Module DragonProxy
+### 
+### Author:  Mark Lillibridge
+### Version: 1.0
+### 
+
+import VocolaUtils
+
+use_send_input = True
+verbose = True
+
+
+def proxy_playString(keys):
+    if use_send_input:
+        if verbose:
+            print("playString->SI("+ repr(keys) + ")")
+        import vocola_ext_keys
+        vocola_ext_keys.send_input(keys)
+    else:
+        # Example of what you can do with this proxy:
+        # # DNS 11.5 @ bug workaround (change @ to shift+2 including in {ctrl+@}):
+        # keys = re.sub(r"""(?x) 
+        #                   \{ ( (?: [a-zA-Z\x80-\xff]+ \+ )* )
+        #                         @
+        #                   ( (?: [ ] \d+)? ) \}""", r'{\1shift+2\2}', keys)
+        # keys = re.sub(r"""@""", "{shift+2}", keys)
+
+        if verbose:
+            print("playString->SDK("+ repr(keys) + ")")
+        VocolaUtils.direct_playString(keys)
+
+def proxy_Dragon(function_name, argument_types, arguments):
+    if verbose:
+        print("Dragon: "+function_name+"("+repr(argument_types)+": "+repr(arguments)+")")
+    VocolaUtils.direct_Dragon(function_name, argument_types, arguments)
+
+
+# Vocola procedure: DragonProxy.SetVerbose
+def set_verbose(verbose_on):
+    global verbose
+    if verbose_on == "true":
+        verbose = True
+    else:
+        verbose = False
+
+# Vocola procedure: DragonProxy.SetUseSendInput
+def set_use_send_input(use_send_input_on):
+    global use_send_input
+    if use_send_input_on == "true":
+        use_send_input = True
+    else:
+        use_send_input = False
