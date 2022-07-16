ba1fcd9ca0ba4f232b1da4e8c67c7a61662b4311
Mark Lillibridge <mdl@alum.mit.edu>
2022-07-16 13:42:33 -0700

    very beginnings of new NatLink backend

---
+++ src/exec/vcl2py/backend_NatLink2.py
@@ -0,0 +1,329 @@
+import re
+from vcl2py.ast import *
+from vcl2py.log import *
+
+def output(out_file, grammar, params):
+    global Grammar, VocolaVersion
+    Grammar = grammar
+    VocolaVersion = params["Vocola_version"]
+    maximum_commands = params["maximum_commands"]
+
+    global OUT
+    try:
+        OUT = open(out_file, "w")
+    except IOError, e:
+        log_error("Unable to open output file '" + out_file + \
+                  "' for writing: " + str(e))
+        return
+
+    emit_file_header()
+
+    executables = grammar["EXECUTABLE"]
+    if len(executables) == 0:
+         # <<<>>>
+        if re.match(r'.*(short|range).*', out_file):
+            emit(0, "#context = (~dragonfly.AppContext(executable='emacs')) & ~dragonfly.AppContext(executable='xwin')\n")
+        else:
+            emit(0, "#context = dragonfly.AppContext()\n")
+    else:
+        # <<<>>>
+        executable = executables[-1]
+        emit(0, "#context = dragonfly.AppContext(executable='" + executable + "')\n")
+    # emit(0, 'context = dragonfly.AppContext(executable="emacs", title="yellow emacs")'+ "\n")
+
+
+    emit(0, "#grammar = dragonfly.Grammar('Test grammar', context=context)\n\n\n")
+    emit(0, "grammar = Grammar()\n\n\n")
+
+
+    global Emitted_Rules
+    Emitted_Rules = set()
+
+    # <<<>>>
+    if () in grammar["CONTEXTS"].keys():
+        rule_names = grammar["CONTEXTS"][()]
+        grammar["RULES"]["0_all"] = one_of(rule_names)
+        if maximum_commands == 1:
+            grammar["CONTEXTS"][()] = ["0_all"]
+        else:
+            grammar["RULES"]["1_all"] = magic_with(repeated_element(rule_ref("0_all"), maximum_commands), maximum_commands)
+            grammar["CONTEXTS"][()] = ["1_all"]
+
+    # don't do title-specific contexts yet:
+    if () in grammar["CONTEXTS"].keys():
+        rule_names = grammar["CONTEXTS"][()]
+        for rule_name in rule_names:
+            emit_rule(rule_name, grammar["RULES"][rule_name], True) 
+
+    # <<<>>>
+    if len(Emitted_Rules) > 0:
+        emit_file_trailer()
+    OUT.close()
+
+def implementation_error(error):
+    log_error(error)
+    raise RuntimeError, error    # <<<>>>
+
+def magic_with(element, count):
+    rule = {}
+    rule["TYPE"] = "with"
+    rule["ELEMENT"] = element
+    rule["ACTIONS"] = [reference_action(i) for i in range(count,0, -1)]
+    return rule
+
+def reference_action(i):
+    action = {}
+    action["TYPE"] = "reference"
+    action["SLOT"] = i
+    return action
+
+def one_of(rule_names):
+    rule = {}
+    rule["TYPE"] = "alternatives"
+    rule["CHOICES"] = [rule_ref(rule_name) for rule_name in rule_names]
+    return rule
+
+def rule_ref(rule_name):
+    rule = {}
+    rule["TYPE"] = "rule_reference"
+    rule["NAME"] = rule_name
+    return rule
+
+def repeated_element(element, count):
+    if count == 1:
+        return slotted_element(element, count)
+    rule = {}
+    rule["TYPE"] = "sequence"
+    rule["ELEMENTS"] = [slotted_element(element, count), 
+                        optional_element(repeated_element(element, count - 1))]
+    return rule
+
+def slotted_element(element, slot):
+    rule = {}
+    rule["TYPE"] = "slot"
+    rule["NUMBER"] = slot
+    rule["ELEMENT"] = element
+    return rule
+
+def optional_element(element):
+    rule = {}
+    rule["TYPE"] = "optional"
+    rule["ELEMENT"] = element
+    return rule
+
+
+# ---------------------------------------------------------------------------
+# Emit dragonfly output
+
+def emit_rule(rule_name, rule, top_level):
+    global Emitted_Rules
+    if rule_name in Emitted_Rules:
+        return
+    Emitted_Rules.add(rule_name)
+    # the next line emits as a side effect any rules we are dependent on:
+    element_code = [code_for_element(rule)]
+    rule_class = "ExportedRule" if top_level else "Rule"
+    element_code = [make_string(rule_name)] + element_code
+    if top_level:
+        element_code = [make_variable("__file__")] + element_code
+    rule_variable = "rule_" + rule_name
+    rule_code = make_assignment(rule_variable, make_call(rule_class, element_code))
+    emit_code(rule_code)
+    if top_level:
+        emit(0, "grammar.add_rule(" + rule_variable + ")\n")
+
+def code_for_element(element):
+    type = element["TYPE"]
+    if type == "terminal":
+        return make_call("Term", [make_string(element["TEXT"])])
+    elif type == "dictation":
+        return make_call("Dictation", [])
+    elif type == "rule_reference":
+        referenced = element["NAME"]
+        emit_rule(referenced, Grammar["RULES"][referenced], False)
+        return make_call("RuleRef", [make_variable("rule_" + referenced)])
+    elif type == "optional":
+        element_code = code_for_element(element["ELEMENT"])
+        return make_call("Opt", [element_code])
+    elif type == "alternatives":
+        codes = [make_list([code_for_element(e) for e in element["CHOICES"]])]
+        return make_call("Alt", codes)
+    elif type == "sequence":
+        codes = [make_list([code_for_element(e) for e in element["ELEMENTS"]])]
+        return make_call("Seq", codes)
+    elif type == "slot":
+        element_code = code_for_element(element["ELEMENT"])
+        return make_call("Slot", [element_code, make_integer(element["NUMBER"])])
+    elif type == "with":
+        element_code = code_for_element(element["ELEMENT"])
+        actions_code = code_for_actions(element["ACTIONS"])
+        return make_call("With", [element_code, actions_code])
+    elif type == "without":
+        element_code = code_for_element(element["ELEMENT"])
+        return make_call("Without", [element_code])
+    else:
+        implementation_error("code_for_element: unknown element type: " + type)
+
+def code_for_actions(actions):
+    return make_list([code_for_action(action) for action in actions])
+
+def code_for_action(action):
+    type = action["TYPE"]
+    if type == "text":
+        return make_string(action["TEXT"])
+    elif type == "reference":
+        return make_call("Ref", [make_integer(action["SLOT"])])
+    elif type == "call":
+        return code_for_call(action)
+    else:
+        implementation_error("Unknown action type: '" + type + "'")
+
+def code_for_call(call):
+    call_type = call["CALLTYPE"]
+    name_code = [make_string(call["NAME"])]
+    module_code = []
+    if "MODULE" in call.keys():
+        module_code = [make_string(call["MODULE"])]
+    argument_code = [code_for_actions(a) for a in call["ARGUMENTS"]]
+    codes = module_code + name_code + argument_code
+    return make_call(call_type_name(call_type), codes)
+
+def call_type_name(call_type):
+    if call_type == "dragon":
+        return "DragonCall"
+    elif call_type == "vocola":
+        return "VocolaCall"
+    elif call_type == "extension_routine":
+        return "ExtRoutine"
+    elif call_type == "extension_procedure":
+        return "ExtProc"
+    else:
+        implementation_error("Unknown call type: '" + call_type + "'")
+
+
+# ---------------------------------------------------------------------------
+# Code for emitting pretty printed Python code
+
+#
+# These are used to generate *code*, which can be later pretty printed:
+#
+
+def make_variable(variable):
+    return make_leaf(variable)
+
+def make_integer(number):
+    return make_leaf(str(number))
+
+def make_string(text):
+    return make_leaf('"' + make_safe_python_string(text) + '"')
+    
+def make_list(codes):
+    return make_branch("[", "]", codes)
+
+def make_call(name, codes):
+    return make_branch(name + "(", ")", codes)
+
+def make_assignment(variable, code):
+    return add_prefix(variable + " = ", code)
+
+
+#
+# These are the underlying primitives used by the above
+#
+
+Indent = 2
+Separator = ", "
+
+def make_leaf(text):
+    return (text, len(text), 0)
+
+def make_branch(head_text, tail_text, codes):
+    size = len(head_text) + len(tail_text)
+    depth = 1
+    if len(codes) > 0:
+        depth = max(c[2] for c in codes) + 1
+        size = size + len(Separator)*(len(codes)-1) + \
+            sum(c[1] for c in codes)
+    return ([head_text, tail_text, codes], size, depth)
+
+def add_prefix(text, code):
+    info, size, depth = code
+    if isinstance(info, str):
+        return (text + info, size + len(text), depth)
+    head_text, tail_text, codes = info
+    return ([text + head_text, tail_text, codes], size + len(text), depth)
+
+def make_safe_python_string(text):
+    text = text.replace("\\", "\\\\")
+    text = text.replace("'", "\\'")
+    text = text.replace("\"", "\\\"")
+    text = text.replace("\n", "\\n")
+    return text
+
+
+#
+# Actual code for emitting
+#
+
+Wrap_column = 100
+
+def emit_code(code, indent=0, trailer=""):
+    #emit(0, flatten_code(code) + "\n")
+    #return
+    info, size, depth = code
+    if isinstance(info, str) or indent*Indent + size < Wrap_column:
+        emit(indent, flatten_code(code) +  trailer + "\n")
+        return
+    head_text, tail_text, codes = info
+    emit(indent, head_text + "\n")
+    for code in codes:
+        emit_code(code, indent + 1, ",")
+    emit(indent, tail_text +  trailer + "\n")
+
+def flatten_code(code):
+    info = code[0]
+    if isinstance(info, str):
+        return info
+    head_text, tail_text, codes = info
+    return head_text + Separator.join([flatten_code(c) for c in codes]) + tail_text
+
+def emit(indent, text):
+    global OUT
+    OUT.write(' ' * (Indent * indent) + text)
+
+
+
+# ---------------------------------------------------------------------------
+# Pieces of the output Python file
+
+def emit_file_header():
+    global VocolaVersion, OUT
+    from time import localtime, strftime
+
+    now = strftime("%a %b %d %H:%M:%S %Y", localtime())
+    print >>OUT, "# NatLink macro definitions for NatLink"
+    print >>OUT, "# coding: latin-1"
+    print >>OUT, "# Generated by vcl2py " + VocolaVersion + ", " + now
+    print >>OUT, '''
+from __future__ import print_function
+
+from vocola_NatLink_runtime import *
+
+
+#
+# Our grammar's rules:
+#
+
+''',
+
+def emit_file_trailer():
+    print >>OUT, '''
+
+print("***** loading " + __file__ + "...")
+grammar.load_grammar()
+def unload():
+    global grammar
+    print("***** unloading " + __file__ + "...")
+    if grammar: grammar.unload_grammar()
+    grammar = None
+''',
---
+++ src/to_core/vocola_NatLink_runtime.py
@@ -0,0 +1,451 @@
+###
+### vocola_NatLink_runtime.py - Code used by Vocola's generated
+###                               Python code produced by the NatLink backend
+###
+
+from __future__ import print_function
+
+import importlib
+import sys
+
+import natlinkutils
+import dragonfly
+
+from VocolaUtils import (VocolaRuntimeError, do_flush, to_long, call_Dragon, eval_template)
+
+
+
+##
+## Element implementation using NatLink
+##
+
+class Empty:
+    def to_NatLink_grammar_element(self):
+        raise undefined
+
+class Term:
+    def __init__(self, terminal_text):
+         self.terminal_text = terminal_text
+
+    def to_NatLink_grammar_element(self):
+        return make_safe_python_string(self.terminal_text)
+
+def make_safe_python_string(text):
+    text = text.replace("\\", "\\\\")
+    text = text.replace("'", "\\'")
+    text = text.replace("\"", "\\\"")
+    text = text.replace("\n", "\\n")
+    return text
+
+class Dictation:
+    def to_NatLink_grammar_element(self):
+        return "<dgndictation>"
+
+class RuleRef:
+    def __init__(self, rule):
+         self.rule = rule
+
+    def to_NatLink_grammar_element(self):
+        return "<" + self.rule.get_name() + ">"
+
+
+class Opt:
+    def __init__(self, element):
+         self.element = element
+
+    def to_NatLink_grammar_element(self):
+        return "[" + self.element.to_NatLink_grammar_element() + "]"
+
+class Alt:
+    def __init__(self, alternatives):
+         self.alternatives = alternatives
+
+    def to_NatLink_grammar_element(self):
+        return "(" + " | ".join(c.to_NatLink_grammar_element() for c in self.alternatives) + ")"
+
+class Seq:
+    def __init__(self, elements):
+         self.elements = elements
+
+    def to_NatLink_grammar_element(self):
+        return " ".join(c.to_NatLink_grammar_element() for c in self.elements)
+
+class Slot:
+    def __init__(self, element, number):
+         self.element = element
+         self.number = number
+
+    def reduce_to_actions(self, value):
+        if value is None:
+            return []
+        if isinstance(value, str):
+            return [Text(value)]
+        if isinstance(value, Action):
+            return [value]
+        if isinstance(value, list):
+            result = []
+            for v in value:
+                result += self.reduce_to_actions(v)
+            return result
+        raise VocolaRuntimeError("Implementation error: " +
+                                 "Slot element received unexpected value from sub element: " +
+                                 repr(value))
+
+    def reduce_to_action(self, value):
+        actions = self.reduce_to_actions(value)
+        return actions[0] if len(actions) > 0 else Text("")
+
+    def to_NatLink_grammar_element(self):
+        return self.element.to_NatLink_grammar_element()
+    # def to_dragonfly(self):
+    #      return dragonfly.Modifier(self.element.to_dragonfly(), 
+    #                                lambda values: (self.number, self.reduce_to_action(values)))
+
+class With:
+    def __init__(self, element, actions):
+         self.element = element
+         self.action = Prog(actions)
+
+    def get_bindings(self, value):
+        bindings = {}
+        if isinstance(value, tuple):
+            bindings[value[0]] = value[1]
+        if isinstance(value, list):
+            for v in value:
+                b = self.get_bindings(v)
+                bindings.update(b)
+        return bindings
+
+    def to_NatLink_grammar_element(self):
+        return self.element.to_NatLink_grammar_element()
+    # def to_dragonfly(self):
+    #      return dragonfly.Modifier(self.element.to_dragonfly(), 
+    #                                lambda value: self.action.bind(self.get_bindings(value)))
+
+class Without:
+    def __init__(self, element):
+         self.element = element
+
+    def reduce_to_actions(self, value):
+        if value is None:
+            return []
+        if isinstance(value, str):
+            return [Text(value)]
+        if isinstance(value, Action):
+            return [value]
+        if isinstance(value, list):
+            result = []
+            for v in value:
+                result += self.reduce_to_actions(v)
+            return result
+        raise VocolaRuntimeError("Implementation error: " +
+                                 "Without element received unexpected value from sub element: " +
+                                 repr(value))
+        
+    def to_NatLink_grammar_element(self):
+        return self.element.to_NatLink_grammar_element()
+    # def to_dragonfly(self):
+    #      return dragonfly.Modifier(self.element.to_dragonfly(), 
+    #                                lambda values: Join(self.reduce_to_actions(values)))
+
+
+#
+# Dealing with formatting dictation:  <<<>>>
+#
+
+def format_words(word_list):
+    word_list = [word.encode('Windows-1252') for word in word_list]
+    format_words2(word_list)  # for print side effect
+    import nsformat
+    state = [nsformat.flag_no_space_next]
+    result, _new_state = nsformat.formatWords(word_list, state)
+    print("format_words: %s -> '%s'"  % (repr(word_list), result))
+    return result
+
+def format_words2(word_list):
+    result = ""
+    for word in word_list:
+        # Convert to written form if necessary, e.g. "@\at-sign" --> "@"
+        backslashPosition = str.find(word, "\\\\")
+        if backslashPosition > 0:
+            word = word[:backslashPosition]
+        if result != "":
+            result = result + " "
+        result = result + word
+    print("format_words2: %s -> '%s'"  % (repr(word_list), result))
+    return result
+
+
+
+##
+## Actions except calls
+##
+
+# note that the type of bindings is Dict of int => Action
+
+class Action:
+    def bind(self, bindings):
+        return BoundAction(bindings, self)
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        return preceding_text
+
+class BoundAction(Action):
+    def __init__(self, bindings, action):
+        self.bindings = bindings
+        self.action = action
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        return self.action.eval(is_top_level, self.bindings, preceding_text)
+
+
+class Text(Action):
+    def __init__(self, text):
+        self.text = text
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        return preceding_text + self.text
+
+class Ref(Action):
+    def __init__(self, slot):
+         self.slot = slot
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        if self.slot in bindings.keys():
+            action = bindings[self.slot]
+        else:
+            action = Text("")
+        return action.eval(is_top_level, bindings, preceding_text)
+
+class Prog(Action):
+    def __init__(self, actions):
+        self.actions = actions
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        for action in self.actions:
+            # For conciseness, Prog allows passing strings instead of Text actions
+            if isinstance(action, str):
+                action = Text(action)
+            preceding_text = action.eval(is_top_level, bindings, preceding_text)
+        return preceding_text
+
+class Join(Action):
+    def __init__(self, actions):
+        self.actions = actions
+
+    def eval(self, is_top_level, bindings, preceding_text):
+        result = preceding_text
+        seen_new_text = False
+        for action in self.actions:
+            new_text = action.eval(False, bindings, "")
+            if new_text != "":
+                if seen_new_text:
+                    result = result + " "
+                else:
+                    seen_new_text = True
+                result = result + new_text
+        return result
+
+
+
+##
+## Actions that call
+##
+
+class ActionCall(Action):
+    def __init__(self, name, *arguments):
+        self.name = name
+        self.arguments = [Prog(actions) for actions in arguments]
+
+
+class DragonCall(ActionCall):
+    def eval(self, is_top_level, bindings, preceding_text):
+        do_flush(not is_top_level, preceding_text)
+        dragon_info = Dragon_functions[self.name][1]
+        values = [argument.eval(False, bindings, "") for argument in self.arguments]
+        call_Dragon(self.name, dragon_info, values)
+        return ""
+
+# Built in Dragon functions with (minimum number of arguments,
+# template of types of all possible arguments); template has one
+# letter per argument with s denoting string and i denoting integer:
+
+Dragon_functions = {
+                     "ActiveControlPick" : [1,"s"],
+                     "ActiveMenuPick"    : [1,"s"],
+                     "AppBringUp"        : [1,"ssis"],
+                     "AppSwapWith"       : [1,"s"],
+                     "Beep"              : [0,""],
+                     "ButtonClick"       : [0,"ii"],
+                     "ClearDesktop"      : [0,""],
+                     "ControlPick"       : [1,"s"],
+                     "DdeExecute"        : [3,"sssi"],
+                     "DdePoke"           : [4,"ssss"],
+                     "DllCall"           : [3,"sss"],
+                     "DragToPoint"       : [0,"i"],
+                     "GoToSleep"         : [0,""],
+                     "HeardWord"         : [1,"ssssssss"],  # max 8 words
+                     "HTMLHelp"          : [2,"sss"],
+                     "MenuCancel"        : [0,""],
+                     "MenuPick"          : [1,"s"],
+                     "MouseGrid"         : [0,"ii"],
+                     "MsgBoxConfirm"     : [3,"sis"],
+                     "PlaySound"         : [1,"s"],
+                     "RememberPoint"     : [0,""],
+                     "RunScriptFile"     : [1,"s"],
+                     "SendKeys"          : [1,"s"],
+                     "SendDragonKeys"    : [1,"s"],
+                     "SendSystemKeys"    : [1,"si"],
+                     "SetMicrophone"     : [0,"i"],
+                     "SetMousePosition"  : [2,"iii"],
+                     "SetNaturalText"    : [1,"i"],
+                     "ShellExecute"      : [1,"siss"],
+                     "ShiftKey"          : [0,"ii"],
+                     "TTSPlayString"     : [0,"ss"],
+                     "Wait"              : [1,"i"],
+                     "WaitForWindow"     : [1,"ssi"],
+                     "WakeUp"            : [0,""],
+                     "WinHelp"           : [2,"sii"],
+                    }
+
+
+class VocolaCall(ActionCall):
+    def eval(self, is_top_level, bindings, preceding_text):
+        name = self.name
+        if   name == "EvalTemplate":
+            values = [argument.eval(False, bindings, "") for argument in self.arguments]
+            return preceding_text + eval_template(*values)
+        elif name == "If":
+            condition = self.arguments[0].eval(False, bindings, "")
+            if condition.lower() == "true":
+                return self.arguments[1].eval(is_top_level, bindings, preceding_text)
+            elif len(self.arguments) > 2:
+                return self.arguments[2].eval(is_top_level, bindings, preceding_text)
+            else:
+                return preceding_text
+        elif name == "When":
+            value = self.arguments[0].eval(False, bindings, "")
+            if value != "":
+                return self.arguments[1].eval(is_top_level, bindings, preceding_text)
+            elif len(self.arguments) > 2:
+                return self.arguments[2].eval(is_top_level, bindings, preceding_text)
+            else:
+                return preceding_text
+        elif name == "Repeat":
+            count = self.arguments[0].eval(False, bindings, "")
+            count = to_long(count)
+            for i in range(count):
+                preceding_text = self.arguments[1].eval(is_top_level, bindings, preceding_text)
+            return preceding_text
+        elif name == "Unimacro":
+            # <<<>>>
+            raise VocolaRuntimeError("Unimacro gateway not yet implemented")
+        else: 
+            raise VocolaRuntimeError("Implementation error: " +
+                                     "VocolaCall passed unknown Vocola function: " +
+                                     name)
+
+
+class ExtensionCall(ActionCall):
+    def __init__(self, module_name, name, *arguments):
+        self.module_name = module_name
+        ActionCall.__init__(self, name, *arguments)
+
+    def get_extension_implementation(self):
+        if self.module_name in sys.modules:
+            extension_module = sys.modules[self.module_name]
+        else:
+            print("automatically importing: " + self.module_name)
+            extension_module = importlib.import_module(self.module_name)
+        extension_routine = getattr(extension_module, self.name)
+        return extension_routine
+
+class ExtRoutine(ExtensionCall):
+    def eval(self, is_top_level, bindings, preceding_text):
+        values = [argument.eval(False, bindings, "") for argument in self.arguments]
+        implementation = self.get_extension_implementation()
+        result = implementation(*values)
+        print(self.module_name + ":" + self.name + "(" +
+              ", ".join([repr(v) for v in values]) + ") -> " + repr( result))
+        return preceding_text + result
+
+class ExtProc(ExtensionCall):
+    def eval(self, is_top_level, bindings, preceding_text):
+        if not is_top_level:
+            raise VocolaRuntimeError(
+                'attempt to call Vocola extension ' +
+                'procedure ' +  self.name + ' in a functional context!')
+        do_flush(not is_top_level, preceding_text)
+        values = [argument.eval(False, bindings, "") for argument in self.arguments]
+        implementation = self.get_extension_implementation()
+        result = implementation(*values)
+        return ""
+
+    
+
+##
+## Rule implementation using 
+##
+
+class Rule():
+    def __init__(self, name_, element_):
+        self.name = name_
+        self.element = element_
+        # dragonfly.Rule.__init__(self, name=name_, element=element_.to_dragonfly(), 
+        #                         exported=False)
+        #print("inner : " + repr(element_.to_dragonfly().gstring()))
+
+    def get_name(self):
+        return self.name
+
+class ExportedRule():
+    def __init__(self, file_, name_, element_):
+        self.name = name_
+        self.element = element_
+        # dragonfly.Rule.__init__(self, name=name_, element=element_.to_dragonfly())
+        self.file = file_
+        # print("loaded from " + file_ + " : " + repr(element_.to_dragonfly().gstring()))
+
+    def process_recognition(self, node):
+        try:
+            print("\nExportedRule " + self.name + " from "+ self.file + " recognized: " + repr(node))
+            action = node.value()
+            text = action.eval(True, {}, "")
+            print("resulting text is <" + text + ">")
+            do_flush(False, text)
+        except Exception as e:
+            print(self.name + " threw exception: " + repr(e))
+            import traceback
+            traceback.print_exc(e)
+
+    def to_NatLink_grammar_element(self):
+        return self.element.to_NatLink_grammar_element()
+
+    def get_name(self):
+        return self.name
+
+
+##
+## 
+##
+
+class Grammar(natlinkutils.GrammarBase):
+    def __init__(self):
+        pass
+
+    def load_grammar(self):
+        self.gramSpec = """
+testing
+"""
+        #self.load(self.gramSpec)
+        self.currentModule = ("","",0)
+        self.rule_state = {}
+
+        pass
+
+    def unload_grammar(self):
+        unload()
+
+    def add_rule(self, rule):
+        print("adding rule with: ")
+        print(rule.to_NatLink_grammar_element())
